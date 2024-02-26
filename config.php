1a693160d39b41ed4df65628beacb9d85ed23c2a
Senz <132201088+senzgt@users.noreply.github.com>
2024-02-26 19:04:11 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Willy Arisky
 $site = isset($_GET['site']) ? $_GET['site']: null;
 $go= base64_decode($site);
-$domain = "http://localhost/safelink";
-$title = "Link Decrypter";
+$domain = "http://linksgo.cfd/safelink";
+$title = "Links Decrypter";
 $feed = "http://citizen.co.id/feed";
 ?>
